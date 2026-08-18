e7595579c0c360e88f9fbb7d6e142177ceff1a1b
Rey Cannavaro <reyjunoalcannavaro@gmail.com>
2026-08-18 15:06:23 +0700

    feat: add custom model for m_prog_pelatihan_d_level with audit fields and joins

--- Models/m_prog_pelatihan_d_level/Custom.php
+++ Models/m_prog_pelatihan_d_level/Custom.php
@@ -14,5 +14,8 @@ class m_prog_pelatihan_d_level extends \App\Models\BasicModels\m_prog_pelatihan_
     public $createAdditionalData = ["creator_id"=>"auth:id"];
     public $updateAdditionalData = ["last_editor_id"=>"auth:id"];
 
-    
+    public $joins = [
+        "m_prog_pelatihan.id=m_prog_pelatihan_d_level.m_prog_pelatihan_id",
+        "m_level_posisi.id=m_prog_pelatihan_d_level.m_level_posisi_id"
+    ];
 }
