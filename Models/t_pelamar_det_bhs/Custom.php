de69bd2376f6990fd728b53dd419834b26203923
Rey Cannavaro <reyjunoalcannavaro@gmail.com>
2026-09-11 14:45:36 +0700

    feat: add model files and migration for t_pelamar_det_bhs table

--- Models/t_pelamar_det_bhs/Custom.php
+++ Models/t_pelamar_det_bhs/Custom.php
@@ -1,7 +1,18 @@
 <?php
 
-/**
- * placeholder model: t_pelamar_det_bhs
- * bagian: Custom
- * tempel source code dari generator lama di file ini.
- */
+namespace App\Models\CustomModels;
+
+class t_pelamar_det_bhs extends \App\Models\BasicModels\t_pelamar_det_bhs
+{    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public $fileColumns    = [ /*file_column*/ ];
+
+    //public $createAdditionalData = ["creator_id"=>"auth:id"];
+    //public $updateAdditionalData = ["last_editor_id"=>"auth:id"];
+
+    
+}
