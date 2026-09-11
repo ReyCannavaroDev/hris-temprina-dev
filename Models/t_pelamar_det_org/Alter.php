8785feef9d32f140ce20ea4f86fc592028c5e812
Rey Cannavaro <reyjunoalcannavaro@gmail.com>
2026-09-11 14:48:51 +0700

    feat: initialize t_pelamar_det_org model, migration, and testing suite

--- Models/t_pelamar_det_org/Alter.php
+++ Models/t_pelamar_det_org/Alter.php
@@ -1,7 +1,19 @@
 <?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-/**
- * placeholder model: t_pelamar_det_org
- * bagian: Alter
- * tempel source code dari generator lama di file ini.
- */
+class tpelamardetorg extends Migration
+{
+    protected $tableName = "t_pelamar_det_org";
+    
+    public function up()
+    {
+        Schema::table($this->tableName, function (Blueprint $table) {
+            //$table->string('_existColumnName_')->change();
+            //$table->string('_columnName_');
+            //$table->dropColumn([ ]);
+            $table->bigInteger('jenis_org_id')->comment('{"src":"m_general.id"}')->nullable()->change();
+        });
+    }
+}
--- Models/t_pelamar_det_org/Custom.php
+++ Models/t_pelamar_det_org/Custom.php
@@ -1,7 +1,18 @@
 <?php
 
-/**
- * placeholder model: t_pelamar_det_org
- * bagian: Custom
- * tempel source code dari generator lama di file ini.
- */
+namespace App\Models\CustomModels;
+
+class t_pelamar_det_org extends \App\Models\BasicModels\t_pelamar_det_org
+{    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public $fileColumns    = [ /*file_column*/ ];
+
+    //public $createAdditionalData = ["creator_id"=>"auth:id"];
+    //public $updateAdditionalData = ["last_editor_id"=>"auth:id"];
+
+    
+}
