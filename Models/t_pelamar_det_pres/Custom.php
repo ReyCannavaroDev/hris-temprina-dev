e5ffdb61ef42d451542c25eb308c8b0df24aa535
Rey Cannavaro <reyjunoalcannavaro@gmail.com>
2026-09-11 14:54:22 +0700

    feat: initialize t_pelamar_det_pres model with database migration, tests, and base configurations

--- Models/t_pelamar_det_pres/Custom.php
+++ Models/t_pelamar_det_pres/Custom.php
@@ -1,7 +1,18 @@
 <?php
 
-/**
- * placeholder model: t_pelamar_det_pres
- * bagian: Custom
- * tempel source code dari generator lama di file ini.
- */
+namespace App\Models\CustomModels;
+
+class t_pelamar_det_pres extends \App\Models\BasicModels\t_pelamar_det_pres
+{    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public $fileColumns    = [ /*file_column*/ ];
+
+    //public $createAdditionalData = ["creator_id"=>"auth:id"];
+    //public $updateAdditionalData = ["last_editor_id"=>"auth:id"];
+
+    
+}
--- Models/t_pelamar_det_pres/Test.php
+++ Models/t_pelamar_det_pres/Test.php
@@ -1,7 +1,58 @@
 <?php
+namespace Tests;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Laravel\Passport\Passport;
+use App\Models\Defaults\User;
 
-/**
- * placeholder model: t_pelamar_det_pres
- * bagian: Test
- * tempel source code dari generator lama di file ini.
- */
+class tPelamarDetPresTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testReadingData()
+    {
+        Passport::actingAs(User::first());
+        $payload = [
+            'paginate' => 25
+        ];
+
+        $this->call('GET', '/operation/t_pelamar_det_pres', $payload);
+
+        $responseArr = json_decode( $this->response->getContent(),true );
+        ff( $responseArr, 'dump data' );
+
+        $this->assertTrue(true);
+    }
+
+    public function testCreatingData()
+    {
+        $user = User::where('username', 'USERNAME')->first();
+        $this->assertNotEmpty( $user );
+        
+        Passport::actingAs($user);
+
+        $payload = [
+		    "id" => "bigint:optional:autocreate",
+		    "t_pelamar_id" => "bigint:optional",
+		    "nama_pres" => "string:191:required",
+		    "tahun" => "integer:required",
+		    "tingkat_pres_id" => "bigint:required",
+		    "desc" => "text:optional",
+		    "creator_id" => "integer:optional",
+		    "last_editor_id" => "integer:optional",
+		    "created_at" => "datetime:optional:autocreate",
+		    "updated_at" => "datetime:optional:autocreate"
+		];
+
+        $this->call('POST', '/operation/t_pelamar_det_pres', $payload);
+
+        $responseArr = json_decode( $this->response->getContent(),true );
+        // ff( $responseArr, 'dump data' );
+
+        $this->assertEquals( 200, $this->response->status() );
+        // $this->seeJsonStructure( ['status'] );
+
+        $this->seeInDatabase('t_pelamar_det_pres', array_filter($payload, function($dt){
+            return !is_array($dt);
+        } ));
+    }
+}
