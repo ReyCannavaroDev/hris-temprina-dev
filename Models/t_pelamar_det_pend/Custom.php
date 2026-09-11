2f5159bae50177719d13a7d83683515cdbaeef2f
Rey Cannavaro <reyjunoalcannavaro@gmail.com>
2026-09-11 14:51:02 +0700

    feat: create model and migration for t_pelamar_det_pend with associated tests

--- Models/t_pelamar_det_pend/Custom.php
+++ Models/t_pelamar_det_pend/Custom.php
@@ -1,7 +1,18 @@
 <?php
 
-/**
- * placeholder model: t_pelamar_det_pend
- * bagian: Custom
- * tempel source code dari generator lama di file ini.
- */
+namespace App\Models\CustomModels;
+
+class t_pelamar_det_pend extends \App\Models\BasicModels\t_pelamar_det_pend
+{    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public $fileColumns    = ['ijazah_foto'];
+
+    //public $createAdditionalData = ["creator_id"=>"auth:id"];
+    //public $updateAdditionalData = ["last_editor_id"=>"auth:id"];
+
+    
+}
--- Models/t_pelamar_det_pend/Test.php
+++ Models/t_pelamar_det_pend/Test.php
@@ -1,7 +1,66 @@
 <?php
+namespace Tests;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Laravel\Passport\Passport;
+use App\Models\Defaults\User;
 
-/**
- * placeholder model: t_pelamar_det_pend
- * bagian: Test
- * tempel source code dari generator lama di file ini.
- */
+class tPelamarDetPendTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testReadingData()
+    {
+        Passport::actingAs(User::first());
+        $payload = [
+            'paginate' => 25
+        ];
+
+        $this->call('GET', '/operation/t_pelamar_det_pend', $payload);
+
+        $responseArr = json_decode( $this->response->getContent(),true );
+        ff( $responseArr, 'dump data' );
+
+        $this->assertTrue(true);
+    }
+
+    public function testCreatingData()
+    {
+        $user = User::where('username', 'USERNAME')->first();
+        $this->assertNotEmpty( $user );
+        
+        Passport::actingAs($user);
+
+        $payload = [
+		    "id" => "bigint:optional:autocreate",
+		    "t_pelamar_id" => "bigint:optional",
+		    "tingkat_id" => "bigint:required",
+		    "nama_sekolah" => "string:191:required",
+		    "tahun_masuk" => "string:191:required",
+		    "tahun_lulus" => "string:191:required",
+		    "kota_id" => "bigint:required",
+		    "nilai" => "decimal:required",
+		    "jurusan" => "string:191:required",
+		    "is_pend_terakhir" => "boolean:required",
+		    "ijazah_no" => "string:191:required",
+		    "ijazah_foto" => "string:191:optional",
+		    "keterangan" => "text:optional",
+		    "is_active" => "boolean:required",
+		    "creator_id" => "bigint:optional",
+		    "last_editor_id" => "bigint:optional",
+		    "created_at" => "datetime:optional:autocreate",
+		    "updated_at" => "datetime:optional:autocreate"
+		];
+
+        $this->call('POST', '/operation/t_pelamar_det_pend', $payload);
+
+        $responseArr = json_decode( $this->response->getContent(),true );
+        // ff( $responseArr, 'dump data' );
+
+        $this->assertEquals( 200, $this->response->status() );
+        // $this->seeJsonStructure( ['status'] );
+
+        $this->seeInDatabase('t_pelamar_det_pend', array_filter($payload, function($dt){
+            return !is_array($dt);
+        } ));
+    }
+}
