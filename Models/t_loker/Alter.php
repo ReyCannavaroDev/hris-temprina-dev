99f3f3617c55fdedab0a7c981f0d68559011ac0d
Rey Cannavaro <reyjunoalcannavaro@gmail.com>
2026-09-11 15:45:51 +0700

    feat: add new schema columns to t_loker and t_pelamar tables via migration files

--- Models/t_loker/Alter.php
+++ Models/t_loker/Alter.php
@@ -3,6 +3,41 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+if (!function_exists('mb_strimwidth')) {
+    function mb_strimwidth($str, $start, $width, $trimmarker = '', $encoding = null) {
+        $str = (string)$str;
+        if (strlen($str) <= $width) return $str;
+        $markerLen = strlen($trimmarker);
+        $sub = substr($str, $start, max(0, $width - $markerLen));
+        return $sub . $trimmarker;
+    }
+}
+if (!function_exists('mb_strlen')) {
+    function mb_strlen($str, $encoding = null) {
+        return strlen((string)$str);
+    }
+}
+if (!function_exists('mb_substr')) {
+    function mb_substr($str, $start, $length = null, $encoding = null) {
+        return $length === null ? substr((string)$str, $start) : substr((string)$str, $start, $length);
+    }
+}
+if (!function_exists('mb_strwidth')) {
+    function mb_strwidth($str, $encoding = null) {
+        return strlen((string)$str);
+    }
+}
+if (!function_exists('mb_ord')) {
+    function mb_ord($str, $encoding = null) {
+        return ord((string)$str);
+    }
+}
+if (!function_exists('mb_chr')) {
+    function mb_chr($code, $encoding = null) {
+        return chr($code);
+    }
+}
+
 class tloker extends Migration
 {
     protected $tableName = "t_loker";
