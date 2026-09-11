f00437af56c723e349978d13f6e00cbb167b203d
Rey Cannavaro <reyjunoalcannavaro@gmail.com>
2026-09-11 14:49:55 +0700

    feat: implement t_pelamar_det_pel model with migration, basic/custom schemas, and unit tests

--- Models/t_pelamar_det_pel/Test.php
+++ Models/t_pelamar_det_pel/Test.php
@@ -1,7 +1,58 @@
 <?php
+namespace Tests;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Laravel\Passport\Passport;
+use App\Models\Defaults\User;
 
-/**
- * placeholder model: t_pelamar_det_pel
- * bagian: Test
- * tempel source code dari generator lama di file ini.
- */
+class tPelamarDetPelTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testReadingData()
+    {
+        Passport::actingAs(User::first());
+        $payload = [
+            'paginate' => 25
+        ];
+
+        $this->call('GET', '/operation/t_pelamar_det_pel', $payload);
+
+        $responseArr = json_decode( $this->response->getContent(),true );
+        ff( $responseArr, 'dump data' );
+
+        $this->assertTrue(true);
+    }
+
+    public function testCreatingData()
+    {
+        $user = User::where('username', 'USERNAME')->first();
+        $this->assertNotEmpty( $user );
+        
+        Passport::actingAs($user);
+
+        $payload = [
+		    "id" => "bigint:optional:autocreate",
+		    "t_pelamar_id" => "bigint:optional",
+		    "nama_pel" => "string:100:required",
+		    "tahun" => "integer:required",
+		    "nama_lem" => "string:100:required",
+		    "kota_id" => "bigint:optional",
+		    "creator_id" => "integer:optional",
+		    "last_editor_id" => "integer:optional",
+		    "created_at" => "datetime:optional:autocreate",
+		    "updated_at" => "datetime:optional:autocreate"
+		];
+
+        $this->call('POST', '/operation/t_pelamar_det_pel', $payload);
+
+        $responseArr = json_decode( $this->response->getContent(),true );
+        // ff( $responseArr, 'dump data' );
+
+        $this->assertEquals( 200, $this->response->status() );
+        // $this->seeJsonStructure( ['status'] );
+
+        $this->seeInDatabase('t_pelamar_det_pel', array_filter($payload, function($dt){
+            return !is_array($dt);
+        } ));
+    }
+}
