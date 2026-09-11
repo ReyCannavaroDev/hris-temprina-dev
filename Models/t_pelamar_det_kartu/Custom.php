b835aa4d603195ae6294265d031f146134129cec
Rey Cannavaro <reyjunoalcannavaro@gmail.com>
2026-09-11 14:47:43 +0700

    feat: create t_pelamar_det_kartu model, migration, and associated test suite

--- Models/t_pelamar_det_kartu/Custom.php
+++ Models/t_pelamar_det_kartu/Custom.php
@@ -1,7 +1,18 @@
 <?php
 
-/**
- * placeholder model: t_pelamar_det_kartu
- * bagian: Custom
- * tempel source code dari generator lama di file ini.
- */
+namespace App\Models\CustomModels;
+
+class t_pelamar_det_kartu extends \App\Models\BasicModels\t_pelamar_det_kartu
+{    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public $fileColumns    = [ /*file_column*/ ];
+
+    //public $createAdditionalData = ["creator_id"=>"auth:id"];
+    //public $updateAdditionalData = ["last_editor_id"=>"auth:id"];
+
+    
+}
--- Models/t_pelamar_det_kartu/Test.php
+++ Models/t_pelamar_det_kartu/Test.php
@@ -1,7 +1,67 @@
 <?php
+namespace Tests;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Laravel\Passport\Passport;
+use App\Models\Defaults\User;
 
-/**
- * placeholder model: t_pelamar_det_kartu
- * bagian: Test
- * tempel source code dari generator lama di file ini.
- */
+class tPelamarDetKartuTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testReadingData()
+    {
+        Passport::actingAs(User::first());
+        $payload = [
+            'paginate' => 25
+        ];
+
+        $this->call('GET', '/operation/t_pelamar_det_kartu', $payload);
+
+        $responseArr = json_decode( $this->response->getContent(),true );
+        ff( $responseArr, 'dump data' );
+
+        $this->assertTrue(true);
+    }
+
+    public function testCreatingData()
+    {
+        $user = User::where('username', 'USERNAME')->first();
+        $this->assertNotEmpty( $user );
+        
+        Passport::actingAs($user);
+
+        $payload = [
+		    "id" => "bigint:optional:autocreate",
+		    "t_pelamar_id" => "bigint:optional",
+		    "ktp_no" => "string:25:optional",
+		    "ktp_foto" => "string:191:optional",
+		    "pas_foto" => "string:191:optional",
+		    "kk_no" => "string:25:optional",
+		    "kk_foto" => "string:191:optional",
+		    "npwp_no" => "string:25:optional",
+		    "npwp_foto" => "string:191:optional",
+		    "npwp_tgl_berlaku" => "date:optional",
+		    "bpjs_tipe_id" => "bigint:optional",
+		    "bpjs_no" => "string:30:optional",
+		    "bpjs_foto" => "string:191:optional",
+		    "berkas_lain" => "string:191:optional",
+		    "desc_file" => "text:optional",
+		    "creator_id" => "integer:optional",
+		    "last_editor_id" => "integer:optional",
+		    "created_at" => "datetime:optional:autocreate",
+		    "updated_at" => "datetime:optional:autocreate"
+		];
+
+        $this->call('POST', '/operation/t_pelamar_det_kartu', $payload);
+
+        $responseArr = json_decode( $this->response->getContent(),true );
+        // ff( $responseArr, 'dump data' );
+
+        $this->assertEquals( 200, $this->response->status() );
+        // $this->seeJsonStructure( ['status'] );
+
+        $this->seeInDatabase('t_pelamar_det_kartu', array_filter($payload, function($dt){
+            return !is_array($dt);
+        } ));
+    }
+}
